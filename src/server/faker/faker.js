592bac5bf011026a5d932b9d575288bf6418e81e
Matt Upham <mcupham@gmail.com>
2018-02-02 19:02:48 -0800

    complete draft of data generation script

--- src/server/faker/faker.js
+++ src/server/faker/faker.js
@@ -1,86 +1,35 @@
 var faker = require("faker");
 var fs = require('fs');
 
-for (var i = 1; i <= 3; i++){
+// var one_million = 1;
+var one_million = 1000000;
+var start = 1;
+var stop = one_million;
 
-  // var file = fs.createWriteStream(`./data${i}.csv`);
-  writeData(1, 20);
-  
-  function writeData(bound1, bound2){
-    var header = 'videoID|publishedAt|title|description|duration|views|thumbnails|videoUrl\n';
-    // file.write(header);
-    // fs.writeFileSync(`./data${i}.csv`, header);
-    
+for (var i = 1; i <= 10; i++){
+  writeVideoInfoData(start, stop);
+  function writeVideoInfoData(start, stop){
+    var header = 'video_id|published_at|title|description|duration|views|video_url|thumbnail_1|thumbnail_2|thumbnail_3|\n';
     var data = header;
-
-    for (var j = bound1; j <= bound2; j++){
-      if (j % 1000000 === 0) { console.log('j: ', j) }
-      var videoID = i.toString();
-      var publishedAt = "2018-01-29T22:39:08+00:00"
+    for (var j = start; j <= stop; j++){
+      if (j % 100000 === 0) { console.log('j: ', j) }
+      var video_id = j;
+      var published_at = "2018-01-29T22:39:08+00:00"
       var title = faker.lorem.sentence();
       var description = faker.lorem.sentence();
       var duration = faker.random.number(10000000);
       var views = faker.random.number(10000000);
-      var thumbnails = {};
-      var videoUrl = `https://www.youtube.com/watch?v=${videoID}`;
-      var csvString = `${videoID}|${publishedAt}|${title}|${description}|${duration}|${views}|${thumbnails}|${videoUrl}\n`;
-      // file.write(csvString);
-      // file.write(JSON.stringify(csvString2));
-      // file.write(csvString3);
+      var video_url = `https://www.youtube.com/watch?v=${video_id}`;
+      var thumbnail_1 = `https://i.ytimg.com/vi/${video_id}/default1.jpg`
+      var thumbnail_2 = `https://i.ytimg.com/vi/${video_id}/default2.jpg`
+      var thumbnail_3 = `https://i.ytimg.com/vi/${video_id}/default3.jpg`
+      var csvString = `${video_id}|${published_at}|${title}|${description}|${duration}|${views}|${video_url}|${thumbnail_1}|${thumbnail_2}|${thumbnail_3}\n`;
       data += csvString;
     }
+    console.log(`now writing file ${i}`)
     fs.writeFileSync(`./data${i}.csv`, data);
-
-    // file.end(function(){console.log('done')});
+    console.log(`done writing file ${i}, ${stop - start} lines`)
   }
+  start += one_million;
+  stop += one_million;
 };
-
-
-
-
-
-
-
-
-
-// file.write(videoID);
-// file.write('|');
-// file.write(publishedAt);
-// file.write('|');
-// file.write(title);
-// file.write('|');
-// file.write(description);
-// file.write('|');
-// file.write(duration);
-// file.write('|');
-// file.write(views);
-// file.write('|');
-// file.write(thumbnails);
-// file.write('|');
-// file.write(videoUrl);
-// var string1 = 'hi';
-// var string2 = '|';
-// var string3 = 'hi2';
-// file.write(string1);
-// file.write(string2);
-// file.write(string3);
-
-/*
-var thumbnails = {
-  default: {
-    url: 'https://i.ytimg.com/vi/0ByoQm-vnYw/default.jpg',
-    width: 120,
-    height: 90
-  },
-  medium: {
-    url: 'https://i.ytimg.com/vi/0ByoQm-vnYw/mqdefault.jpg',
-    width: 320,
-    height: 180
-  },
-  high: {
-    url: 'https://i.ytimg.com/vi/0ByoQm-vnYw/hqdefault.jpg',
-    width: 480,
-    height: 360
-  }
-};
-*/
--- src/server/faker/upload.sql
+++ src/server/faker/upload.sql
@@ -1,9 +1,16 @@
-`COPY videos FROM 'faker/data.csv' DELIMITER '|' HEADER true`;
+\copy video_info FROM 'faker/data1.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data2.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data3.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data4.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data5.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data6.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data7.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data8.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data9.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy video_info FROM 'faker/data10.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
 
+-- SELECT COUNT(*) FROM videos;
+-- SELECT videos FROM videos WHERE videos.videoID = '1';
+-- CREATE INDEX video_id ON videos;
 
-`\copy videos FROM 'faker/data.csv' DELIMITER '|' HEADER true`;
-
-`\copy videos FROM 'faker/data.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'))`;
-
-
-\copy videos FROM 'faker/data.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+INSERT INTO video_info VALUES (10000001, '"2018-01-29T22:39:08+00:00"', 'Title', 'Description', 10, 20, 'https://www.youtube.com/watch?v=1', 'https://i.ytimg.com/vi/1/default1.jpg', 'https://i.ytimg.com/vi/2/default2.jpg', 'https://i.ytimg.com/vi/3/default3.jpg');
